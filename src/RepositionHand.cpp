9105bee414591ce1f8675165a0f2e5f253b151c9
idkm23@gmail.com <idkm23@gmail.com>
2015-08-05 15:46:37 -0400

    Documented RepositionHand.cpp.

--- src/RepositionHand.cpp
+++ src/RepositionHand.cpp
@@ -1,9 +1,13 @@
+/* This program calls the IK Position Solver service used for moving the arm to desired coordinates. */
+
 #include "RepositionHand.h"
 
 const int RepositionHand::RIGHT = 0;
 const int RepositionHand::LEFT = 1;
 
-void RepositionHand::begin_detection()
+/* Starts up the callback functions. */
+void 
+RepositionHand::begin_detection()
 {
     ros::spin();
 }
@@ -23,7 +27,9 @@ RepositionHand::RepositionHand()
     isMoving = false;
 }
 
-bool RepositionHand::callback(operation_plushie::RepositionHand::Request &req, operation_plushie::RepositionHand::Response &res)
+/* Sets up the message for the desired position and calls the IK Solver service, using returned info to see if the position is possible. */
+bool 
+RepositionHand::callback(operation_plushie::RepositionHand::Request &req, operation_plushie::RepositionHand::Response &res)
 {
     isLeft = req.isLeft;
     needsConsistency = req.needsConsistency;
@@ -33,7 +39,8 @@ bool RepositionHand::callback(operation_plushie::RepositionHand::Request &req, o
     
     baxter_core_msgs::SolvePositionIK srv;
 
-    //start of math functions to setup pose
+    //The following math functions take a Roll, Pitch and Yaw and convert them to the XYZW for orientation.
+    //These Roll, Pitch and Yaw values correspond to the hand pointing straight down.
     const double ROLL = 0, PITCH = 3.14, YAW = req.yaw;
 
     double mathc1 = cos(PITCH),
@@ -51,13 +58,17 @@ bool RepositionHand::callback(operation_plushie::RepositionHand::Request &req, o
     oriy = (maths1 * mathc2 + maths1 * mathc3 + mathc1 * maths2 * maths3) / oriw4;
     oriz = (-maths1 * maths3 + mathc1 * maths2 * mathc3 + maths2) / oriw4;
 
+    //This time stamp should be set to current time.
     ps.header.frame_id = req.frame; 
     ps.header.stamp = ros::Time::now();
 
+    //Sets the XYZ position of the desired pose to the XYZ values from the request.
+    //The x-value should not be greater than .84 or it'll be too far for Baxter to reach.
     ps.pose.position.x = (req.x > .84 ? .84 : req.x);
     ps.pose.position.y = req.y;
     ps.pose.position.z = req.z;
 
+    //XYZW of orientation is set to the calculated values for each.
     ps.pose.orientation.x = orix;
     ps.pose.orientation.y = oriy;
     ps.pose.orientation.z = oriz;
@@ -79,6 +90,7 @@ bool RepositionHand::callback(operation_plushie::RepositionHand::Request &req, o
         return true;
     }
    
+    //Sets arm motion to position mode.
     msg.mode = 1;
     
     for(int i = 0; i < srv.response.joints[0].name.size(); i++)
@@ -94,19 +106,25 @@ bool RepositionHand::callback(operation_plushie::RepositionHand::Request &req, o
     return true;
 }
 
-void RepositionHand::updateLeftEndpoint(baxter_core_msgs::EndpointState eps)
+/* Updates the position of Baxter's left hand. */
+void 
+RepositionHand::updateLeftEndpoint(baxter_core_msgs::EndpointState eps)
 {
     if(isLeft)
         updateEndpoint(eps);
 }
 
-void RepositionHand::updateRightEndpoint(baxter_core_msgs::EndpointState eps)
+/* Updates the position of Baxter's right hand. */
+void 
+RepositionHand::updateRightEndpoint(baxter_core_msgs::EndpointState eps)
 {
     if(!isLeft)
         updateEndpoint(eps);
 }
 
-void RepositionHand::updateEndpoint(baxter_core_msgs::EndpointState eps)
+/* Checks the position and orientation of desired endpoint. Checks if the hand gets stuck while it's trying to move. */
+void 
+RepositionHand::updateEndpoint(baxter_core_msgs::EndpointState eps)
 {
     const double P_WIG = .001f, O_WIG = .05f;
     if(fabs(eps.pose.position.x - c_x) < P_WIG && fabs(eps.pose.position.y - c_y) < P_WIG && fabs(eps.pose.position.z - c_z) < P_WIG 
@@ -137,6 +155,7 @@ void RepositionHand::updateEndpoint(baxter_core_msgs::EndpointState eps)
         
         joint_pub[(isLeft ? LEFT : RIGHT)].publish(msg);        
         
+        //If the hand can't move, reassign variables to reflect that Baxter's hand is stuck.
         if(needsConsistency && consistent_torque_count > 15 && c_pose_count > 15 && ps.pose.position.z < eps.pose.position.z)
         {
             ROS_INFO("final torque: %f", s1_torque);
@@ -146,7 +165,9 @@ void RepositionHand::updateEndpoint(baxter_core_msgs::EndpointState eps)
     }
 }
 
-bool RepositionHand::isPositioned(baxter_core_msgs::EndpointState eps)
+/* Check if the hand is in the correct position. Return true if this is true. */
+bool 
+RepositionHand::isPositioned(baxter_core_msgs::EndpointState eps)
 {
     const double P_WIG = .007f, O_WIG = .05f;
     
@@ -155,7 +176,9 @@ bool RepositionHand::isPositioned(baxter_core_msgs::EndpointState eps)
 
 }
 
-void RepositionHand::updateEffort(sensor_msgs::JointState js)
+/* Updates the value of the shoulder's torque and checks if it's been at a consistent value. */
+void 
+RepositionHand::updateEffort(sensor_msgs::JointState js)
 {
     int indexSpacing = (isLeft ? 0 : 7);
 
@@ -171,7 +194,9 @@ void RepositionHand::updateEffort(sensor_msgs::JointState js)
     }
 }
 
-bool RepositionHand::progressCallback(operation_plushie::isComplete::Request &req, operation_plushie::isComplete::Response &res)
+/* Updates variables that reflect the progress of the motion. */
+bool 
+RepositionHand::progressCallback(operation_plushie::isComplete::Request &req, operation_plushie::isComplete::Response &res)
 {
     res.isComplete = !isMoving;
     res.isStuck = isStuck;
